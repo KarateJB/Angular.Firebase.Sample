83b5f74200d8978c9a745b7faf20bacbd0db1f5d
KarateJB <ppippenkimo@gmail.com>
2018-03-17 11:50:05 +0800

    Move all functions to fbService

--- sample/src/app/component/login/login.component.ts
+++ sample/src/app/component/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
 import { User } from '@firebase/auth-types';
+import { FbService } from '../../service/fb.service';
 
 @Component({
   selector: 'app-login',
@@ -10,65 +11,20 @@ import { User } from '@firebase/auth-types';
 })
 export class LoginComponent implements OnInit {
 
-    private isAuth = false;
-    private user: User = null;
-
-    constructor(private afAuth: AngularFireAuth) {
-        
-        this.afAuth.authState.subscribe(
-            user => this.changeState(user),
-            error => console.trace(error)
-        );
-
+    constructor(private fbService: FbService) {
     }
 
     ngOnInit() {
 
+    }    
+    
+    login(provider: string){
+        this.fbService.login(provider);
     }
 
-    private login(provider: string) {
-        let afProvider = this.getProvider(provider);
-        this.afAuth.auth.signInWithPopup(afProvider);
-      }
-
-      logout() {
-        this.afAuth.auth.signOut();
-      }
-      
-    private changeState(user: any = null) {
-        console.log(JSON.stringify(user));
-        if (user) {
-            this.isAuth = true;
-            // this.user = this.getUserInfo(user);
-            this.user = user;
-        }
-        else {
-            this.isAuth = false;
-            this.user = null;
-        }
+    logout(){
+        this.fbService.logout();
     }
 
-    private getUserInfo(user: any): any {
-        if (!user) {
-            return {};
-        }
-        console.info(user);
-
-        let data = user.auth.providerData[0];
-        return {
-            name: data.displayName,
-            avatar: data.photoURL,
-            email: data.email,
-            provider: data.providerId
-        };
-    }
-
-    private getProvider(provider: string) {
-        switch (provider) {
-            case 'twitter': new firebase.auth.TwitterAuthProvider();
-            case 'facebook': new firebase.auth.FacebookAuthProvider();
-            case 'github': return new firebase.auth.GithubAuthProvider();
-            case 'google': return new firebase.auth.GoogleAuthProvider();
-        }
-    }
+    
 }
--- sample/src/app/service/fb.service.ts
+++ sample/src/app/service/fb.service.ts
@@ -1,14 +1,26 @@
-import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 import { User } from '@firebase/auth-types';
+
 
 @Injectable()
 export class FbService {
-  
+
   public isAuth: boolean = false;
   public user: User = null;
   constructor(private afAuth: AngularFireAuth) {
+    this.afAuth.authState.subscribe(
+      user => this._changeState(user),
+      error => console.trace(error)
+    );
   }
+
+  public login(provider: string) {
+    let afProvider = this._getProvider(provider);
+    this.afAuth.auth.signInWithPopup(afProvider);
+  }
+
 
   public logout() {
     this.afAuth.auth.signOut();
@@ -25,14 +37,23 @@ export class FbService {
   private _changeState(user: any = null) {
     console.log(JSON.stringify(user));
     if (user) {
-        this.isAuth = true;
-        // this.user = this.getUserInfo(user);
-        this.user = user;
+      this.isAuth = true;
+      // this.user = this.getUserInfo(user);
+      this.user = user;
     }
     else {
-        this.isAuth = false;
-        this.user = null;
+      this.isAuth = false;
+      this.user = null;
     }
-}
+  }
+
+  private _getProvider(provider: string) {
+    switch (provider) {
+      case 'twitter': new firebase.auth.TwitterAuthProvider();
+      case 'facebook': new firebase.auth.FacebookAuthProvider();
+      case 'github': return new firebase.auth.GithubAuthProvider();
+      case 'google': return new firebase.auth.GoogleAuthProvider();
+    }
+  }
 
 }
