f41f3290a19f1b9eb6034306f900327cbb23fd5f
KarateJB <ppippenkimo@gmail.com>
2018-11-11 00:34:50 +0800

    Refactoring

--- sample/src/app/class/FirebaseConfig.ts
+++ sample/src/app/class/FirebaseConfig.ts
@@ -2,12 +2,12 @@ export class FirebaseConfig {
 
   get config(): any {
     return {
-      apiKey: "AIzaSyCZzDE0KS0jj4WIW424-92w86lerFaLiKU",
-      authDomain: "fir-e1a22.firebaseapp.com",
-      databaseURL: "https://fir-e1a22.firebaseio.com",
-      projectId: "fir-e1a22",
-      storageBucket: "fir-e1a22.appspot.com",
-      messagingSenderId: "499014936125"
+      apiKey: "xxxxxxxxxxxxxxxxxx",
+      authDomain: "xxxxxxxxxxxxxx.com",
+      databaseURL: "https://xxxxxxxxxxx.com",
+      projectId: "xxxxxxxxxxxx",
+      storageBucket: "xxxxxxxxxxxx.appspot.com",
+      messagingSenderId: "xxxxxxxxxxxxx"
     };
   }
 }
