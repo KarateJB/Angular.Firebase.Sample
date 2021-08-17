2cd6da1641f2988adac688404ac16449258f477e
KarateJB <ppippenkimo@gmail.com>
2021-08-17 18:31:32 +0800

    Add module

--- src/src/app/class/configs/firebase-config.ts
+++ src/src/app/class/configs/firebase-config.ts
@@ -1,2 +1,12 @@
 export class FirebaseConfig {
+  get config(): any {
+    return {
+      apiKey: 'xxxxxxxxxxxxxxxxxx',
+      authDomain: 'xxxxxxxxxxxxxx.com',
+      databaseURL: 'https://xxxxxxxxxxx.com',
+      projectId: 'xxxxxxxxxxxx',
+      storageBucket: 'xxxxxxxxxxxx.appspot.com',
+      messagingSenderId: 'xxxxxxxxxxxxx',
+    };
+  }
 }
--- src/src/app/class/configs/toastr-config.ts
+++ src/src/app/class/configs/toastr-config.ts
@@ -1,2 +1,10 @@
-export class ToastrConfig {
-}
+import { ToastOptions } from "ng2-toastr";
+export class ToastConfig extends ToastOptions {
+  dismiss: 'auto';
+  animate = 'flyRight'; // you can override any options available
+  positionClass: 'toast-bottom-right';
+  showCloseButton: false;
+};
+
+
+
